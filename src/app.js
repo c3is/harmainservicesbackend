c9f572e750e97a5fbd086fe09a8a03eebb03fc62
Aqib Bashir <itzaqibbashir@gmail.com>
2025-12-22 17:16:23 +0530

    added fixes

--- src/app.js
+++ src/app.js
@@ -60,7 +60,7 @@ app.get("/services",async(req,res)=>
 {
     try{
         const services=await ServiceModel.find({});
-        res.send(services);
+         res.json(services);
 
     }catch(err){
         res.send("something went wrong")
@@ -70,8 +70,8 @@ app.get("/services",async(req,res)=>
 app.get("/service/:slug",async(req,res)=>{
     try{
         const {slug}=req.params;
-        const doc = await ServiceModel.findOne({slug})
-        res.send(doc);
+        const service = await ServiceModel.findOne({slug})
+        res.json(service);
     }
     catch{
         res.status(401).send("document not found")
